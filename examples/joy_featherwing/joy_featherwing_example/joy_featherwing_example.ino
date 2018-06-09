d4e6dc25993c865811f613b101a06f70c26f2378
ladyada <limor@ladyada.net>
2018-06-08 20:20:38 -0400

    update example

--- examples/joy_featherwing/joy_featherwing_example/joy_featherwing_example.ino
+++ examples/joy_featherwing/joy_featherwing_example/joy_featherwing_example.ino
@@ -1,5 +1,7 @@
 #include "Adafruit_seesaw.h"
+
 Adafruit_seesaw ss;
+
 #define BUTTON_RIGHT 6
 #define BUTTON_DOWN  7
 #define BUTTON_LEFT  9
@@ -7,6 +9,7 @@ Adafruit_seesaw ss;
 #define BUTTON_SEL   14
 uint32_t button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) | 
                 (1 << BUTTON_LEFT) | (1 << BUTTON_UP) | (1 << BUTTON_SEL);
+
 #if defined(ESP8266)
   #define IRQ_PIN   2
 #elif defined(ESP32)
@@ -20,24 +23,33 @@ uint32_t button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
 #else
   #define IRQ_PIN   5
 #endif
+
 void setup() {
   Serial.begin(115200);
-  while(!Serial);
+
+  while(!Serial) {
+    delay(10);
+  }
+
+  Serial.println("Joy FeatherWing example!");
   
   if(!ss.begin(0x49)){
-    Serial.println("ERROR!");
+    Serial.println("ERROR! seesaw not found");
     while(1);
-  }
-  else{
+  } else {
     Serial.println("seesaw started");
     Serial.print("version: ");
     Serial.println(ss.getVersion(), HEX);
   }
   ss.pinModeBulk(button_mask, INPUT_PULLUP);
   ss.setGPIOInterrupts(button_mask, 1);
+
   pinMode(IRQ_PIN, INPUT);
 }
+
+
 int last_x = 0, last_y = 0;
+
 void loop() {
   int x = ss.analogRead(2);
   int y = ss.analogRead(3);
@@ -48,9 +60,12 @@ void loop() {
     last_y = y;
   }
   
-  if(!digitalRead(IRQ_PIN)){
+  /* if(!digitalRead(IRQ_PIN)) {  // Uncomment to use IRQ */
+
     uint32_t buttons = ss.digitalReadBulk(button_mask);
+
     //Serial.println(buttons, BIN);
+
     if (! (buttons & (1 << BUTTON_RIGHT))) {
       Serial.println("Button A pressed");
     }
@@ -61,11 +76,11 @@ void loop() {
       Serial.println("Button Y pressed");
     }
     if (! (buttons & (1 << BUTTON_UP))) {
-      Serial.println("Button x pressed");
+      Serial.println("Button X pressed");
     }
     if (! (buttons & (1 << BUTTON_SEL))) {
       Serial.println("Button SEL pressed");
     }
-  }
+  /* } // Uncomment to use IRQ */
   delay(10);
 }
