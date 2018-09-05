7994a896d61938c38628ae163572c1bc41e6e29d
ladyada <limor@ladyada.net>
2018-09-04 22:05:02 -0400

    add multi-board example (for testing mostly)

--- examples/RGBTrellis/basic/basic.ino
+++ examples/RGBTrellis/basic/basic.ino
@@ -2,6 +2,62 @@
 
 Adafruit_RGBTrellis trellis;
 
+//define a callback for key presses
+TrellisCallback blink(keyEvent evt){
+  // Check is the pad pressed?
+  if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
+    trellis.pixels.setPixelColor(evt.bit.NUM, Wheel(map(evt.bit.NUM, 0, trellis.pixels.numPixels(), 0, 255))); //on rising
+  } else if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {
+  // or is the pad released?
+    trellis.pixels.setPixelColor(evt.bit.NUM, 0); //off falling
+  }
+
+  // Turn on/off the neopixels!
+  trellis.pixels.show();
+
+  return 0;
+}
+
+void setup() {
+  Serial.begin(9600);
+  //while(!Serial);
+  
+  if (!trellis.begin()) {
+    Serial.println("Could not start trellis, check wiring?");
+    while(1);
+  } else {
+    Serial.println("NeoPixel Trellis started");
+  }
+
+  //activate all keys and set callbacks
+  for(int i=0; i<RGB_TRELLIS_NUM_KEYS; i++){
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING);
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING);
+    trellis.registerCallback(i, blink);
+  }
+
+  //do a little animation to show we're on
+  for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i, Wheel(map(i, 0, trellis.pixels.numPixels(), 0, 255)));
+    trellis.pixels.show();
+    delay(50);
+  }
+  for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i, 0x000000);
+    trellis.pixels.show();
+    delay(50);
+  }
+}
+
+void loop() {
+  trellis.read();  // interrupt management does all the work! :)
+  
+  delay(20); //the trellis has a resolution of around 60hz
+}
+
+
+/******************************************/
+
 // Input a value 0 to 255 to get a color value.
 // The colors are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
@@ -15,53 +71,4 @@ uint32_t Wheel(byte WheelPos) {
    return trellis.pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
   return 0;
-}
-
-//define a callback for key presses
-TrellisCallback blink(keyEvent evt){
-  
-  if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING)
-    trellis.pixels.setPixelColor(evt.bit.NUM, Wheel(map(evt.bit.NUM, 0, trellis.pixels.numPixels(), 0, 255))); //on rising
-  else if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING)
-    trellis.pixels.setPixelColor(evt.bit.NUM, 0); //off falling
-    
-  trellis.pixels.show();
-  return 0;
-}
-
-void setup() {
-  Serial.begin(9600);
-  //while(!Serial);
-  
-  if(!trellis.begin()){
-    Serial.println("could not start trellis");
-    while(1);
-  }
-  else{
-    Serial.println("trellis started");
-  }
-
-  //activate all keys and set callbacks
-  for(int i=0; i<RGB_TRELLIS_NUM_KEYS; i++){
-    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING);
-    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING);
-    trellis.registerCallback(i, blink);
-  }
-
-  //do a little animation to show we're on
-  for(uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
-    trellis.pixels.setPixelColor(i, Wheel(map(i, 0, trellis.pixels.numPixels(), 0, 255)));
-    trellis.pixels.show();
-    delay(50);
-  }
-  for(uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
-    trellis.pixels.setPixelColor(i, 0x000000);
-    trellis.pixels.show();
-    delay(50);
-  }
-}
-
-void loop() {
-  trellis.read();
-  delay(20); //the trellis has a resolution of around 60hz
 }
