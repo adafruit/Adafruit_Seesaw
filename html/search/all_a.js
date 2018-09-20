f0a64279752add215d4ad096e33b73ec853ee98c
Travis CI <travis@travis-ci.org>
2018-09-20 21:34:43 +0000

    Deploy code docs to GitHub Pages Travis build: 74
    Commit: 59f9953f8a07af59b2bf63937351649c36b9af83

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['read',['read',['../class_adafruit___neo_trellis.html#ae005a7e4a60d47a4318108f8cfbbb3ad',1,'Adafruit_NeoTrellis::read()'],['../class_adafruit___multi_trellis.html#a6751a46aaef0680cd9827e3be4185fb1',1,'Adafruit_MultiTrellis::read()'],['../class_adafruit__seesaw.html#a43a334b6825afe1a06412e0d77211e64',1,'Adafruit_seesaw::read()'],['../classseesaw___servo.html#a5dd2c1884750a08e563eb2a1c4c21725',1,'seesaw_Servo::read()']]],
-  ['read8',['read8',['../class_adafruit__seesaw.html#a1051e42a96ad3e64eca87a15727924cd',1,'Adafruit_seesaw']]],
+  ['read',['read',['../class_adafruit___neo_trellis.html#a58512ca86c0cb84a9f9869cb447d7ba4',1,'Adafruit_NeoTrellis::read()'],['../class_adafruit___multi_trellis.html#a6751a46aaef0680cd9827e3be4185fb1',1,'Adafruit_MultiTrellis::read()'],['../class_adafruit__seesaw.html#a43a334b6825afe1a06412e0d77211e64',1,'Adafruit_seesaw::read()'],['../classseesaw___servo.html#a5dd2c1884750a08e563eb2a1c4c21725',1,'seesaw_Servo::read()']]],
+  ['read8',['read8',['../class_adafruit__seesaw.html#ab82eeeb63039cf9f01318835a9426ecc',1,'Adafruit_seesaw']]],
   ['readbuttons',['readButtons',['../class_adafruit__mini_t_f_t_wing.html#a6c5dd6ee0eaf73b3a1a6d0486c6806f5',1,'Adafruit_miniTFTWing::readButtons()'],['../class_adafruit___t_f_t_shield18.html#a5b3fe3f77292abe20d76bef2b74b134f',1,'Adafruit_TFTShield18::readButtons()']]],
   ['readkeypad',['readKeypad',['../class_adafruit__seesaw.html#a5bdccf93d008cc94bc67e622f365f3c5',1,'Adafruit_seesaw']]],
   ['readmicroseconds',['readMicroseconds',['../classseesaw___servo.html#a94d2cf30748460ba8bfb39aabf95efc2',1,'seesaw_Servo']]],
