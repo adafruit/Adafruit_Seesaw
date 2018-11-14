1601f69f270eaab1fb031ba1ff8cb4a9e2145518
dean <deanm1278@gmail.com>
2018-11-14 14:57:50 -0500

    DM: soil sensor example

--- examples/soil_sensor/soilsensor_example/soilsensor_example.ino
+++ examples/soil_sensor/soilsensor_example/soilsensor_example.ino
@@ -5,13 +5,9 @@ Adafruit_seesaw ss;
 void setup() {
   Serial.begin(115200);
 
-  while(!Serial) {
-    delay(10);
-  }
-
   Serial.println("seesaw Soil Sensor example!");
   
-  if (!ss.begin(0x49)) {
+  if (!ss.begin(0x36)) {
     Serial.println("ERROR! seesaw not found");
     while(1);
   } else {
@@ -19,9 +15,6 @@ void setup() {
     Serial.println(ss.getVersion(), HEX);
   }
 }
-
-
-int last_x = 0, last_y = 0;
 
 void loop() {
   uint16_t tempC = ss.getTemp();
