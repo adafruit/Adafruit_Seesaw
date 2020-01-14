967f19eef7cd8174f90fa49b79786c59472ac9b7
Doxygen CI <travis@travis-ci.org>
2020-01-14 02:16:53 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adafruit_20seesaw_20arduino_20driver',['Adafruit seesaw arduino driver',['../index.html',1,'']]],
-  ['adafruit_5fseesaw',['Adafruit_Seesaw',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_5fseesaw_20_21_5bbuild_20status_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fadafruit_5fseesaw_2fworkflows_2farduino_2520library_2520ci_2fbadge_2esvg_29',['Adafruit_Seesaw ![Build Status](https://github.com/adafruit/Adafruit_Seesaw/workflows/Arduino%20Library%20CI/badge.svg)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
