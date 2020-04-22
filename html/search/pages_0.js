a6088954f43e38b185a5b7d806c44ac5fe8a5dda
Doxygen CI <travis@travis-ci.org>
2020-04-22 02:00:14 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adafruit_20seesaw_20arduino_20driver',['Adafruit seesaw arduino driver',['../index.html',1,'']]],
-  ['adafruit_5fseesaw_20_21_5bbuild_20status_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fadafruit_5fseesaw_2fworkflows_2farduino_2520library_2520ci_2fbadge_2esvg_29',['Adafruit_Seesaw ![Build Status](https://github.com/adafruit/Adafruit_Seesaw/workflows/Arduino%20Library%20CI/badge.svg)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_5fseesaw_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fadafruit_5fseesaw_2fworkflows_2farduino_2520library_2520ci_2fbadge_2esvg_29_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fadafruit_5fseesaw_2factions_29_5b_21_5bdocumentation_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fci_2darduino_2fblob_2fmaster_2fassets_2fdoxygen_5fbadge_2esvg_29_5d_28http_3a_2f_2fadafruit_2egithub_2eio_2fadafruit_5fseesaw_2fhtml_2findex_2ehtml_29',['Adafruit_Seesaw [![Build Status](https://github.com/adafruit/Adafruit_Seesaw/workflows/Arduino%20Library%20CI/badge.svg)](https://github.com/adafruit/Adafruit_Seesaw/actions)[![Documentation](https://github.com/adafruit/ci-arduino/blob/master/assets/doxygen_badge.svg)](http://adafruit.github.io/Adafruit_Seesaw/html/index.html)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
