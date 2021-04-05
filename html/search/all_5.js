a425ed1531e56663b456945ec8532d8c7327b40e
Doxygen CI <ci-arduino@invalid>
2021-04-05 00:05:12 +0000

    Deploy docs to GitHub Pages from commit 3a987bb31c
    Commit: 3a987bb31ce690b716e2c6b602ebcde00372f305
    GitHub Actions run: 717704635

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['getencoderdelta',['getEncoderDelta',['../class_adafruit__seesaw.html#ae4db818e643fad4f52cec0a4d4e33bdb',1,'Adafruit_seesaw']]],
-  ['getencoderposition',['getEncoderPosition',['../class_adafruit__seesaw.html#a79bb662a47c49736440fe745d19b4958',1,'Adafruit_seesaw']]],
+  ['getencoderdelta',['getEncoderDelta',['../class_adafruit__seesaw.html#a5c3d5b2d93a0a10c62b7817e57e59e6f',1,'Adafruit_seesaw']]],
+  ['getencoderposition',['getEncoderPosition',['../class_adafruit__seesaw.html#a19dfac93da4a6c9330ba965f199f3081',1,'Adafruit_seesaw']]],
   ['geti2caddr',['getI2CAddr',['../class_adafruit__seesaw.html#a5bd7da9dad57027004caf4fc2a3d5d72',1,'Adafruit_seesaw']]],
   ['getkeypadcount',['getKeypadCount',['../class_adafruit__seesaw.html#ab7e771551219cab31104a3d278cdd02c',1,'Adafruit_seesaw']]],
   ['getoptions',['getOptions',['../class_adafruit__seesaw.html#aa9b2e4d4ab1166863a3013c11e0ec606',1,'Adafruit_seesaw']]],
