9d295bbc521669df0319ca0894cbb9ae006bb251
lady ada <limor@ladyada.net>
2021-04-26 23:03:52 -0400

    increase delay for cap read on retries

--- Adafruit_seesaw.cpp
+++ Adafruit_seesaw.cpp
@@ -321,12 +321,14 @@ uint16_t Adafruit_seesaw::touchRead(uint8_t pin) {
   uint8_t buf[2];
   uint8_t p = pin;
   uint16_t ret = 65535;
-  do {
-    delay(1);
-    this->read(SEESAW_TOUCH_BASE, SEESAW_TOUCH_CHANNEL_OFFSET + p, buf, 2,
-               1000);
-    ret = ((uint16_t)buf[0] << 8) | buf[1];
-  } while (ret == 65535);
+
+  for (uint8_t retry = 0; retry < 5; retry++) {
+    if (this->read(SEESAW_TOUCH_BASE, SEESAW_TOUCH_CHANNEL_OFFSET + p, buf, 2,
+                   3000 + retry * 1000)) {
+      ret = ((uint16_t)buf[0] << 8) | buf[1];
+      break;
+    }
+  }
   return ret;
 }
 
