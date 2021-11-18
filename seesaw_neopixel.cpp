e2ed74d5e1b3546ce368979c42ad1e8ce257d4bd
Phillip Burgess <paintyourdragon@dslextreme.com>
2021-11-17 16:42:41 -0800

    Fix a clang-format thing

--- seesaw_neopixel.cpp
+++ seesaw_neopixel.cpp
@@ -37,7 +37,8 @@ seesaw_NeoPixel::seesaw_NeoPixel(uint16_t n, uint8_t p, neoPixelType t,
                                  TwoWire *Wi)
 #endif
     : Adafruit_seesaw(Wi), begun(false), numLEDs(n), pin(p), brightness(0),
-      pixels(NULL), endTime(0), type(t) {}
+      pixels(NULL), endTime(0), type(t) {
+}
 
 // via Michael Vogt/neophob: empty constructor is used when strand length
 // isn't known at compile-time; situations where program config might be
