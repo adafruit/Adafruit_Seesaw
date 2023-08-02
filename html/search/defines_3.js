7634532d679477c72d561061f64cd183f0957811
Doxygen CI <ci-arduino@invalid>
2023-08-02 16:20:04 +0000

    Deploy docs to GitHub Pages from commit 32ad723923
    Commit: 32ad72392354deb6982d417a226f596678b60a94
    GitHub Actions run: 5741188324

--- html/search/defines_3.js
+++ html/search/defines_3.js
@@ -3,5 +3,10 @@ var searchData=
   ['seesaw_5faddress',['SEESAW_ADDRESS',['../_adafruit__seesaw_8h.html#ae995cbda508547cc9342525a83a15718',1,'Adafruit_seesaw.h']]],
   ['seesaw_5feeprom_5fi2c_5faddr',['SEESAW_EEPROM_I2C_ADDR',['../_adafruit__seesaw_8h.html#a29230c31db27fba752397eb80a1d0025',1,'Adafruit_seesaw.h']]],
   ['seesaw_5fhw_5fid_5fcode_5fsamd09',['SEESAW_HW_ID_CODE_SAMD09',['../_adafruit__seesaw_8h.html#afb9de23064ef8be798406922fd4f07bd',1,'Adafruit_seesaw.h']]],
-  ['seesaw_5fhw_5fid_5fcode_5ftiny8x7',['SEESAW_HW_ID_CODE_TINY8X7',['../_adafruit__seesaw_8h.html#a0fc922e913fd62aa7eb2a0b9f5fb439c',1,'Adafruit_seesaw.h']]]
+  ['seesaw_5fhw_5fid_5fcode_5ftiny1616',['SEESAW_HW_ID_CODE_TINY1616',['../_adafruit__seesaw_8h.html#a8c26c7329d6554b464b148101cec9949',1,'Adafruit_seesaw.h']]],
+  ['seesaw_5fhw_5fid_5fcode_5ftiny1617',['SEESAW_HW_ID_CODE_TINY1617',['../_adafruit__seesaw_8h.html#a4a9631177b4474fa18ddd081a3c79dbe',1,'Adafruit_seesaw.h']]],
+  ['seesaw_5fhw_5fid_5fcode_5ftiny806',['SEESAW_HW_ID_CODE_TINY806',['../_adafruit__seesaw_8h.html#a0599ccc89c92ee514b505cb3623f2b97',1,'Adafruit_seesaw.h']]],
+  ['seesaw_5fhw_5fid_5fcode_5ftiny807',['SEESAW_HW_ID_CODE_TINY807',['../_adafruit__seesaw_8h.html#aa4f1ec6160e6dae927760b247e3a82ca',1,'Adafruit_seesaw.h']]],
+  ['seesaw_5fhw_5fid_5fcode_5ftiny816',['SEESAW_HW_ID_CODE_TINY816',['../_adafruit__seesaw_8h.html#a3b20692c3cca667c0090a937fa0f5113',1,'Adafruit_seesaw.h']]],
+  ['seesaw_5fhw_5fid_5fcode_5ftiny817',['SEESAW_HW_ID_CODE_TINY817',['../_adafruit__seesaw_8h.html#a545eaf7d920ba41e5cd4f8f32de89f5f',1,'Adafruit_seesaw.h']]]
 ];
