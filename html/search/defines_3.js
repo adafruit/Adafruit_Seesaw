f7847ef89d32913b413766ef5485da7c2e0af1e9
Doxygen CI <ci-arduino@invalid>
2023-10-27 05:24:01 +0000

    Deploy docs to GitHub Pages from commit c1cd6b5a51
    Commit: c1cd6b5a51fefb313c7ce7d33d5a65a4bbc25203
    GitHub Actions run: 6663127372

--- html/search/defines_3.js
+++ html/search/defines_3.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['seesaw_5faddress',['SEESAW_ADDRESS',['../_adafruit__seesaw_8h.html#ae995cbda508547cc9342525a83a15718',1,'Adafruit_seesaw.h']]],
-  ['seesaw_5feeprom_5fi2c_5faddr',['SEESAW_EEPROM_I2C_ADDR',['../_adafruit__seesaw_8h.html#a29230c31db27fba752397eb80a1d0025',1,'Adafruit_seesaw.h']]],
   ['seesaw_5fhw_5fid_5fcode_5fsamd09',['SEESAW_HW_ID_CODE_SAMD09',['../_adafruit__seesaw_8h.html#afb9de23064ef8be798406922fd4f07bd',1,'Adafruit_seesaw.h']]],
   ['seesaw_5fhw_5fid_5fcode_5ftiny1616',['SEESAW_HW_ID_CODE_TINY1616',['../_adafruit__seesaw_8h.html#a8c26c7329d6554b464b148101cec9949',1,'Adafruit_seesaw.h']]],
   ['seesaw_5fhw_5fid_5fcode_5ftiny1617',['SEESAW_HW_ID_CODE_TINY1617',['../_adafruit__seesaw_8h.html#a4a9631177b4474fa18ddd081a3c79dbe',1,'Adafruit_seesaw.h']]],
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -6,6 +6,7 @@ var searchData=
   ['getencoderdelta',['getEncoderDelta',['../class_adafruit__seesaw.html#a5c3d5b2d93a0a10c62b7817e57e59e6f',1,'Adafruit_seesaw']]],
   ['getencoderposition',['getEncoderPosition',['../class_adafruit__seesaw.html#a19dfac93da4a6c9330ba965f199f3081',1,'Adafruit_seesaw']]],
   ['geti2caddr',['getI2CAddr',['../class_adafruit__seesaw.html#a5bd7da9dad57027004caf4fc2a3d5d72',1,'Adafruit_seesaw']]],
+  ['geti2caddreepromloc',['getI2CaddrEEPROMloc',['../class_adafruit__seesaw.html#a2be6821454e5a36a0f849ee0b8645232',1,'Adafruit_seesaw']]],
   ['getkeypadcount',['getKeypadCount',['../class_adafruit__seesaw.html#ab7e771551219cab31104a3d278cdd02c',1,'Adafruit_seesaw']]],
   ['getlevel',['getLevel',['../classseesaw___audio___spectrum.html#aed95de69c6dfb3e1f82b30e390311ab9',1,'seesaw_Audio_Spectrum']]],
   ['getoptions',['getOptions',['../class_adafruit__seesaw.html#aa9b2e4d4ab1166863a3013c11e0ec606',1,'Adafruit_seesaw']]],
