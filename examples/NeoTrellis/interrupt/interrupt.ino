c328a76a818083a82d52694192a2bbb087b05d8f
nikhilt0203 <nst7766@nyu.edu>
2026-02-09 13:10:06 +0100

    Update interrupt.ino
    fixed blink() function signature

--- examples/NeoTrellis/interrupt/interrupt.ino
+++ examples/NeoTrellis/interrupt/interrupt.ino
@@ -24,7 +24,7 @@ uint32_t Wheel(byte WheelPos) {
 }
 
 //define a callback for key presses
-TrellisCallback blink(keyEvent evt){
+void blink(keyEvent evt){
   
   if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING)
     trellis.pixels.setPixelColor(evt.bit.NUM, Wheel(map(evt.bit.NUM, 0, trellis.pixels.numPixels(), 0, 255))); //on rising
@@ -32,7 +32,6 @@ TrellisCallback blink(keyEvent evt){
     trellis.pixels.setPixelColor(evt.bit.NUM, 0); //off falling
     
   trellis.pixels.show();
-  return 0;
 }
 
 void setup() {
